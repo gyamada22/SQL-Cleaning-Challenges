1afe00a94df7b1cecd2e0d99d8c26db38f7efb26
gyamada22 <guilhermeyamada@poli.ufrj.br>
2025-12-28 13:25:40 -0300

    Update 01_bronze_layer.sql

--- Projetos/01_Tech_Layoffs/SQL_Snowflake_scripts/01_bronze_layer.sql
+++ Projetos/01_Tech_Layoffs/SQL_Snowflake_scripts/01_bronze_layer.sql
@@ -1,1 +1,17 @@
+USE ROLE ACCOUNTADMIN;
 
+-- criar tabelas com varchar para nao ter erro
+CREATE OR REPLACE TABLE SQL_CHALLENGES.BRONZE.STG_LAYOFFS_RAW (
+    company VARCHAR,
+    location VARCHAR,
+    industry VARCHAR,
+    total_laid_off VARCHAR, 
+    percentage_laid_off VARCHAR,
+    date VARCHAR,
+    stage VARCHAR,
+    country VARCHAR,
+    funds_raised_millions VARCHAR
+);
+
+-- teste inicial
+SELECT * FROM SQL_CHALLENGES.BRONZE.STG_LAYOFFS_RAW LIMIT 10;
